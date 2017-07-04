4162afe06a1ce3ed533645eafb7d0875c3983a83
Chris Muir <chrismuirRVA@gmail.com>
2017-07-04 11:22:38 -0400

    remove arg ssl_verify_peer, comtradr back on CRAN

--- global.R
+++ global.R
@@ -1,5 +1,3 @@
-devtools::install_github("ChrisMuir/comtradr")
-
 library(shiny)
 library(plotly)
 library(dplyr)
@@ -7,6 +5,5 @@ library(comtradr)
 
 # Download the countries table and commodities table. Server.R will use both of 
 # these data frames, ui.R will use a number of vectors taken from these data frames.
-countrydf <- comtradr::ct_countries_table(ssl_verify_peer = FALSE)
-commoditydf <- comtradr::ct_commodities_table(type = "HS", 
-                                              ssl_verify_peer = FALSE)
+countrydf <- comtradr::ct_countries_table()
+commoditydf <- comtradr::ct_commodities_table(type = "HS")
